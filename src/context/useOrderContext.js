c1ebae10b34324d34f9d761ed06e6fa9fa52f9bb
uma <uma@tigeen.ch>
2025-03-26 03:30:01 +0530

    order context added

---
+++ src/context/useOrderContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const OrderContext = createContext();
+
+const OrderContextProvider = ({ children }) => {
+  const [state, setState] = useState({});
+
+  return (
+    <OrderContext.Provider value={{ ...state, setState, showError, hideError }}>
+      {children}
+    </OrderContext.Provider>
+  );
+};
+export const useOrderContext = () => {
+  const context = useContext(OrderContext);
+  if (!context) {
+    throw new Error("Error using Context");
+  }
+  return context;
+};
+export default OrderContextProvider;
