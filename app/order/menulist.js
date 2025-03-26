6703b739dc53097e75b6c3eedd3bd6e93637832e
uma <uma@tigeen.ch>
2025-03-26 10:21:22 +0530

    toggle visibility state and bottom arrow button added

--- app/order/menulist.js
+++ app/order/menulist.js
@@ -1,16 +1,51 @@
 import moderateScale from "@/src/utils/responsiveScale";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Pressable } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Themes } from "@/src/utils/themes";
+import { useState } from "react";
 const MenuList = () => {
+  const [categoryVisible, setCategoryVisible] = useState(false);
+  const toggleCategoryVisibility = () => setCategoryVisible((prev) => !prev);
   return (
     <View style={styles.container}>
       <ScrollView>{}</ScrollView>
+      <View
+        style={{
+          backgroundColor: Themes.primary,
+          paddingVertical: moderateScale(5),
+          height: moderateScale(40),
+        }}
+      >
+        <Pressable
+          style={styles.bottomArrowContainer}
+          onPress={toggleCategoryVisibility}
+        >
+          {!categoryVisible ? (
+            <AntDesign name="arrowup" size={35} color="black" />
+          ) : (
+            <AntDesign name="arrowdown" size={35} color="black" />
+          )}
+        </Pressable>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: moderateScale(10),
+  },
+  bottomArrowContainer: {
+    alignSelf: "center",
+    borderRadius: moderateScale(30),
+    borderColor: Themes.black,
+    borderWidth: moderateScale(2),
+    backgroundColor: Themes.white,
+    position: "absolute",
+    bottom: 0,
+    height: moderateScale(60),
+    width: moderateScale(60),
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 export default MenuList;
