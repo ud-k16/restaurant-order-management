9bf3dcba8d8c2ed9f017c8da2ed74d48884c9a91
uma <uma@tigeen.ch>
2025-03-30 02:14:42 +0530

    tempory list of table items in order summary

--- app/order/orderSummary.js
+++ app/order/orderSummary.js
@@ -1,6 +1,23 @@
-import { View, StyleSheet } from "react-native";
+import { useOrderContext } from "@/src/context/useOrderContext";
+import { View, StyleSheet, ScrollView, Text } from "react-native";
 const OrderSummary = ({ tableId }) => {
-  return <View style={styles.container}></View>;
+  const { orders } = useOrderContext();
+  const orderOfTheTable = orders.get(tableId);
+  return (
+    <View style={styles.container}>
+      {orderOfTheTable && (
+        <ScrollView>
+          {orderOfTheTable.map((product) => {
+            return (
+              <View>
+                <Text>{product.productName}</Text>
+              </View>
+            );
+          })}
+        </ScrollView>
+      )}
+    </View>
+  );
 };
 const styles = StyleSheet.create({
   container: {
