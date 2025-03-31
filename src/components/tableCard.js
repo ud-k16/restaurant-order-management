7636a1031a93da26804e2bdfbcf7344e884934b1
uma <uma@tigeen.ch>
2025-03-31 11:34:22 +0530

    dinign text added

--- src/components/tableCard.js
+++ src/components/tableCard.js
@@ -2,7 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 import moderateScale from "@/src/utils/responsiveScale";
 import { Themes } from "@/src/utils/themes";
 import { Link } from "expo-router";
+import { useOrderContext } from "../context/useOrderContext";
 const TableCard = ({ tableId, onLongPress = () => {} }) => {
+  const { orders } = useOrderContext();
   return (
     <Link
       href={{ pathname: "/order/menulist", params: { tableId } }}
@@ -10,6 +12,7 @@ const TableCard = ({ tableId, onLongPress = () => {} }) => {
     >
       <View style={styles.container}>
         <Text>{tableId}</Text>
+        <Text>{orders.has(tableId) && "Dining"}</Text>
       </View>
     </Link>
   );
