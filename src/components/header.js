79271a85457140973f6d49485388a1940341c890
uma <uma@tigeen.ch>
2025-04-02 07:58:59 +0530

    some title change,settings menu added

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,28 @@ import moderateScale from "../utils/responsiveScale";
 import { Themes } from "../utils/themes";
 import { Text } from "react-native";
 import { useHeaderContext } from "../context/useHeaderContext";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
 const AppHeader = () => {
   const { currentTable = "", currentPage } = useHeaderContext();
   return (
     <View style={styles.container}>
       <Text style={styles.titleTextStyle}>
         {currentPage
-          ? `${currentPage} ( Table No. ${currentTable} )`
+          ? currentTable
+            ? `${currentPage} ( Table No. ${currentTable} )`
+            : `${currentPage}`
           : "  Order Management"}
       </Text>
+      <Ionicons
+        name="settings-outline"
+        size={24}
+        color={Themes.white}
+        style={{ flex: 0.2 }}
+        onPress={() => {
+          router.navigate("/Configure");
+        }}
+      />
     </View>
   );
 };
@@ -19,12 +32,15 @@ const styles = StyleSheet.create({
   container: {
     height: moderateScale(50),
     backgroundColor: Themes.primary,
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    flexDirection: "row",
   },
   titleTextStyle: {
     color: Themes.white,
     fontSize: moderateScale(20),
     paddingLeft: moderateScale(15),
+    flex: 1,
   },
 });
 export default AppHeader;
