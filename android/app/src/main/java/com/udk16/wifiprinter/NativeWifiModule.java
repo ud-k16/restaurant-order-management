b17c9b2be780bea849d8824d02b6fbe848053f4e
uma <uma@tigeen.ch>
2025-04-13 16:37:39 +0530

    print sucess

--- android/app/src/main/java/com/udk16/wifiprinter/NativeWifiModule.java
+++ android/app/src/main/java/com/udk16/wifiprinter/NativeWifiModule.java
@@ -11,6 +11,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.net.Socket;
+import android.widget.Toast;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Arguments;
@@ -43,12 +44,18 @@ public static final String NAME = "NativeWifiPrinter";
         OutputStream outputStream = null;
         try {
              byte[] printData = new byte[escposBytes.size()];
+              
         for (int i = 0; i < escposBytes.size(); i++) {
             printData[i] = (byte) escposBytes.getInt(i);
         }
             int printerPort = (int) port;
             InetAddress printerIP = InetAddress.getByName(ipAddress);
             InetSocketAddress address = new InetSocketAddress(printerIP, printerPort);
+            // Convert InetAddress to String
+            String ipAddressString = printerIP.getHostAddress();
+
+            System.out.println("IP Address: " + ipAddressString);
+            
             socket = new Socket();
             socket.connect(address, 10000); // Timeout of 5 seconds
             outputStream = socket.getOutputStream();
@@ -59,6 +66,12 @@ public static final String NAME = "NativeWifiPrinter";
         callback.invoke(map);
         } catch (IOException e) {
             // Log.e(TAG, "Error printing bill via Wi-Fi", e);
+                       System.out.println("error: " + e);
+                       // Get the error message from the IOException
+    String errorMessage = e.getMessage();
+ Toast.makeText(getReactApplicationContext(),
+                errorMessage, Toast.LENGTH_SHORT)
+                .show();
               //writing result to map
              map.putBoolean("success", false);
             // invokes callback to send the map to js
