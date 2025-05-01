02cad774cbec9e98ee9fb3a83fc1c30dcd8c0911
uma <uma@tigeen.ch>
2025-05-01 10:08:19 +0530

    bLUETOOTH page paired device listed

--- app/printerConnectivity/connectToBluetoothPrinter.js
+++ app/printerConnectivity/connectToBluetoothPrinter.js
@@ -1,12 +1,40 @@
 import { View, StyleSheet } from "react-native";
 import { useBluetoothContext } from "@/src/context/useBluetoothContext";
+import Loader from "@/app/Loader";
+import EmptyContent from "@/app/EmptyContent";
+import { useEffect } from "react";
+import { Text } from "react-native";
 const BluetoothPrintScreen = () => {
   const {
     isLoading,
-    pairedDevices: [],
+    pairedDevices = [],
     getPairedBluetoothDevices,
   } = useBluetoothContext();
-  return <View style={styles.container}></View>;
+
+  useEffect(() => {
+    getPairedBluetoothDevices();
+  }, []);
+
+  const ListDevices = () => {
+    return (
+      <View>
+        {pairedDevices.length > 0 ? (
+          <View>
+            {pairedDevices?.map((device, index) => {
+              return <Text key={index}>{device.deviceName}</Text>;
+            })}
+          </View>
+        ) : (
+          <EmptyContent />
+        )}
+      </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      {isLoading ? <Loader /> : <ListDevices />}
+    </View>
+  );
 };
 const styles = StyleSheet.create({
   container: {
