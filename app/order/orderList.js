9625fc7103bcc703374ea96ece783daaf64d94c4
uma <uma@tigeen.ch>
2025-05-03 05:15:48 +0530

    section modal

--- app/order/orderList.js
+++ app/order/orderList.js
@@ -1,13 +1,51 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView, Pressable } from "react-native";
 import { useOrderContext } from "@/src/context/useOrderContext";
+import moderateScale from "@/src/utils/responsiveScale";
+import { Text } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useState } from "react";
 const OrderList = () => {
   const { orders } = useOrderContext();
-  const orderAvailable = orders.entries();
-  return <View style={styles.container}>{}</View>;
+  const orderAvailable = Array.from(orders.entries());
+
+  return (
+    <View style={styles.container}>
+      {orderAvailable.map((data, index) => {
+        const [modalVisible, setModalVisible] = useState(false);
+        const showModal = () => setModalVisible(true);
+        const hideModal = () => setModalVisible(false);
+        const toggleVisibility = () => setModalVisible((prev) => !prev);
+        return (
+          <ScrollView key={index}>
+            <Pressable style={styles.orderCard} onPress={toggleVisibility}>
+              <Text style={styles.orderHeadingText}>{data[0]}</Text>
+              {modalVisible ? (
+                <MaterialIcons name="arrow-drop-up" size={24} color="black" />
+              ) : (
+                <MaterialIcons name="arrow-drop-down" size={24} color="black" />
+              )}
+            </Pressable>
+          </ScrollView>
+        );
+      })}
+    </View>
+  );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  orderCard: {
+    height: moderateScale(50),
+    borderBottomWidth: moderateScale(1),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  orderHeadingText: {
+    // textAlignVertical: "center",
+    fontSize: moderateScale(16),
+    // textAlign: "center",
   },
 });
 export default OrderList;
