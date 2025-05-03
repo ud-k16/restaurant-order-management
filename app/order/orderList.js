e393c6da88a30357f900305e0b4d0e5eb750d949
uma <uma@tigeen.ch>
2025-05-03 05:52:41 +0530

    empty content display

--- app/order/orderList.js
+++ app/order/orderList.js
@@ -1,6 +1,7 @@
 import { View, StyleSheet, ScrollView, Pressable } from "react-native";
 import { useOrderContext } from "@/src/context/useOrderContext";
 import moderateScale from "@/src/utils/responsiveScale";
+import EmptyContent from "@/app/EmptyContent";
 import OrderSummary from "@/app/order/orderSummary";
 import { Text } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
@@ -11,43 +12,39 @@ const OrderList = () => {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {orderAvailable.map((data, index) => {
-          const [modalVisible, setModalVisible] = useState(false);
-          const showModal = () => setModalVisible(true);
-          const hideModal = () => setModalVisible(false);
-          const toggleVisibility = () => setModalVisible((prev) => !prev);
-          return (
-            <View key={index}>
-              <Pressable style={styles.orderCard} onPress={toggleVisibility}>
-                <Text style={styles.orderHeadingText}>{data[0]}</Text>
-                {modalVisible ? (
-                  <MaterialIcons name="arrow-drop-up" size={24} color="black" />
-                ) : (
-                  <MaterialIcons
-                    name="arrow-drop-down"
-                    size={24}
-                    color="black"
-                  />
-                )}
-              </Pressable>
-              {modalVisible && (
-                // <View>
-                //   {data[1].map((product, index) => {
-                //     return (
-                //       <View key={index}>
-                //         <Text>{product.productName}</Text>
-                //       </View>
-                //     );
-                //   })}
-                // </View>
-                <OrderSummary tableId={data[0]} />
-              )}
-              {/* </ScrollView> */}
-            </View>
-          );
-        })}
-      </ScrollView>
+      {orderAvailable.length > 0 ? (
+        <ScrollView>
+          {orderAvailable.map((data, index) => {
+            const [modalVisible, setModalVisible] = useState(false);
+            const showModal = () => setModalVisible(true);
+            const hideModal = () => setModalVisible(false);
+            const toggleVisibility = () => setModalVisible((prev) => !prev);
+            return (
+              <View key={index}>
+                <Pressable style={styles.orderCard} onPress={toggleVisibility}>
+                  <Text style={styles.orderHeadingText}>{data[0]}</Text>
+                  {modalVisible ? (
+                    <MaterialIcons
+                      name="arrow-drop-up"
+                      size={24}
+                      color="black"
+                    />
+                  ) : (
+                    <MaterialIcons
+                      name="arrow-drop-down"
+                      size={24}
+                      color="black"
+                    />
+                  )}
+                </Pressable>
+                {modalVisible && <OrderSummary tableId={data[0]} />}
+              </View>
+            );
+          })}
+        </ScrollView>
+      ) : (
+        <EmptyContent content={"No Orders"} />
+      )}
     </View>
   );
 };
